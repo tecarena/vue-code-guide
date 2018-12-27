3725100ce254f5b56874a77568d494bf7efc87b3
worldale <1265218242@qq.com>
2018-12-27 18:47:56 +0800

    guide

---
+++ src/views/pageGuidePure.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="page">
+        <div class="div1">demo</div>
+    </div>
+</template>
+
+<script>
+    import Upload from '@/components/Plupload'
+    import Dialog from '@/components/Dialog'
+
+    export default {
+
+        data() {
+            return {
+                autoConfig: {
+                    searchForm: {
+                        isPrice: false,
+                        checkMyLove: []
+                    }
+                }
+            }
+        },
+
+        components: {
+            Upload,
+            Dialog
+        },
+
+        beforeCreate() {
+
+        },
+
+        created() {
+
+        },
+
+        beforeMount() {
+
+        },
+
+        mounted() {
+
+        },
+
+        beforeUpdate() {
+
+        },
+
+        updated() {
+
+        },
+
+        beforeDestroy() {
+
+        },
+
+        destroyed() {
+
+        },
+
+        methods: {
+            add() {
+
+            },
+
+            del() {
+
+            },
+
+            edit() {
+                this.nextTick(() => {
+                    // doSomething
+                })
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .page {
+        .div1 {
+            font-size: 12px;
+        }
+    }
+</style>
